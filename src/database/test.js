e004e27e6175d6ff3f692388d6d5659583e721a0
Eugênio Carvalho <eugenio9008@gmail.com>
2020-09-11 19:07:45 -0300

    subject change

--- src/database/test.js
+++ src/database/test.js
@@ -11,7 +11,7 @@ Database.then(async (db) => {
   }
 
   classValue = {
-    subject: "Química",
+    subject: 1,
     cost: "20",
   }
 
@@ -23,11 +23,34 @@ Database.then(async (db) => {
     },
 
     {
-      weekday: 2,
+      weekday: 0,
       time_from: 520,
       time_to: 1220
     }
   ]
 
-  await createProffy(db, { proffyValue, classValue, classScheduleValue })
+  // await createProffy(db, { proffyValue, classValue, classScheduleValue })
+
+  const selectedProffys = await db.all("SELECT * FROM proffys")
+  // console.log(selectedProffys)
+
+  const selectedClassesAndProffys = await db.all(`
+    SELECT classes.*, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE classes.proffy_id = 1;
+  `)
+
+  // console.log(selectedClassesAndProffys)
+
+  const selectedClassesSchedule = await db.all(`
+    SELECT class_schedule.*
+    FROM class_schedule
+    WHERE class_schedule.class_id = "1"
+    AND class_schedule.weekday = "0"
+    AND class_schedule.time_from <= "600"
+    AND class_schedule.time_to > "600"
+  `)
+
+  console.log(selectedClassesSchedule)
 })
